38934024ed230f4c7956722bdcb36d20f32c55d1
daniyalkarim0340 <daniyalkarim70@gmail.com>
2026-05-14 16:05:22 +0500

    gernate access token from refreshtoken

--- backend/route/route.js
+++ backend/route/route.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { register } from "../controllar/auth.controllar.js";
+import {
+  register,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+} from "../controllar/auth.controllar.js";
 
-const authroute=Router();
+const authroute = Router();
 
-authroute.post("/register",register)
-// authroute.post("/login",Lgoinuser)
+// -------------------
+// AUTH ROUTES
+// -------------------
+authroute.post("/register", register);
+authroute.post("/login", loginUser);
+authroute.post("/logout", logoutUser);
+authroute.post("/refresh-token", refreshAccessToken);
 
 export default authroute;
